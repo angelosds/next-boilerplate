638549a002a753f0f922cecaede52d68905906d8
Angelo Santos <angelo.santos@navita.com.br>
2020-08-30 16:35:13 -0300

    Added main content and styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React avançado escrito ao lado"
+    />
+    <S.Title>React avançado</S.Title>
+    <S.Description>Typescript, React, Next e Styled components.</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para a tela com um código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
