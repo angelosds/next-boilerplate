e2ff6a62daf03755e9362514c8989cb7390fb363
Angelo Santos <angelo.santos@navita.com.br>
2020-08-30 23:42:25 -0300

    Added storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  description = 'Typescript, React, Next e Styled components',
+  title = 'React avançado'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React avançado escrito ao lado"
     />
-    <S.Title>React avançado</S.Title>
-    <S.Description>Typescript, React, Next e Styled components.</S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para a tela com um código"
